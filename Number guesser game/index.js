8d9f1a10d63d1223ace465abb64e81224fc47749
anshul yadav <anshulgamers60@gmail.com>
2026-05-29 14:07:58 +0530

    added NUmber guesser game

---
+++ Number guesser game/index.js
@@ -0,0 +1,21 @@
+const t = document.getElementById("inp");
+const btn = document.getElementById("btn");
+const res = document.getElementById("result");
+const min = 1;
+const max = 100;
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+btn.addEventListener("click", () => {
+  let guess = Number(t.value);
+
+  if (isNaN(guess) || guess < min || guess > max) {
+    res.innerText = `Please enter a valid number between ${min} and ${max}`;
+    return;
+  }
+  if (guess === randomNum) {
+    res.innerText = "Congratulations! You guessed the number!";
+  } else if (guess < randomNum) {
+    res.innerText = "Too low! Try again.";
+  } else {
+    res.innerText = "Too high! Try again.";
+  }
+});
